b0f249a7b85f8ba72baec48425c4385f83230964
Ian Rae <ian.rae@snhu.edu>
2024-01-01 13:43:30 -0500

    Vercel Test

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,3 +1,30 @@
+// const { MongoClient } = require("mongodb");
+// const Db = process.env.ATLAS_URI;
+// const client = new MongoClient(Db, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// let _db;
+
+// module.exports = {
+//   connectToServer: async function (callback) {
+//     try {
+//       await client.connect();
+//       console.log("Connected to MongoDB");
+//     } catch (e) {
+//       console.error(e);
+//     }
+
+//     _db = client.db("nba-pre-order");
+
+//     return _db === undefined ? false : true;
+//   },
+//   getDb: function () {
+//     return _db;
+//   },
+// };
+
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
@@ -8,18 +35,17 @@ const client = new MongoClient(Db, {
 let _db;
 
 module.exports = {
-  connectToServer: async function (callback) {
-    try {
-      await client.connect();
-      console.log("Connected to MongoDB");
-    } catch (e) {
-      console.error(e);
-    }
-
-    _db = client.db("nba-pre-order");
-
-    return _db === undefined ? false : true;
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Verify we got a good "db" object
+      if (db) {
+        _db = db.db("nba-pre-order");
+        console.log("Successfully connected to MongoDB.");
+      }
+      return callback(err);
+    });
   },
+
   getDb: function () {
     return _db;
   },
